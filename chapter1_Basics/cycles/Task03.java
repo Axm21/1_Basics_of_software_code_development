1ca942396be5cc6b8911f4bb90c77e087598bf00
AxMakei <97761185+Axm21@users.noreply.github.com>
2022-02-19 22:08:28 +0300

    Add files via upload

---
+++ chapter1_Basics/cycles/Task03.java
@@ -0,0 +1,21 @@
+package chapter1_Basics.cycles;
+
+public class Task03 {
+
+	public static void main(String[] args) {
+		// Найти сумму квадратов первых ста чисел.
+
+		int n;
+		int sumSq;
+
+		sumSq = 0;
+		n = 100;
+
+		for (int i = 1; i <= n; i++) {
+			sumSq += Math.pow(i, 2);
+		}
+		System.out.println(sumSq);
+
+	}
+
+}
---
+++ chapter1_Basics/cycles/Task07.java
@@ -0,0 +1,58 @@
+package chapter1_Basics.cycles;
+
+import java.util.Scanner;
+
+public class Task07 {
+
+	public static void main(String[] args) {
+		// Для каждого натурального числа в промежутке от m до n вывести все делители,
+		// кроме единицы и самого числа. m и n вводятся с клавиатуры.
+		
+		int m;
+		int n;
+		int div;
+
+		m = EntFrCons("m >> ");
+		while (m < 1) {
+			m = EntFrCons("m >> ");
+		}
+		
+		n = EntFrCons("n >> ");
+		while (n < 1) {
+			n = EntFrCons("n >> ");
+		}
+
+		while (m <= n) {
+			System.out.print("dividers for " + m + ": ");
+			div = 2; // начинаем с 2, чтоб исключить 1
+			boolean check = false; // с её помощью будем определять, были ли делитель у числа
+			while (div < m) {
+				if (m % div == 0) {
+					System.out.print(div + "; ");
+					check = true;
+				}
+				div += 1;
+			}
+			if (check == false)
+				System.out.print(" - ");
+			System.out.println(" ");
+			m += 1;
+		}
+
+	}
+
+	public static int EntFrCons(String message) {
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		int value;
+		System.out.print(message);
+		while (!sc.hasNextInt()) {
+			sc.next();
+			System.out.print(message);
+		}
+		value = sc.nextInt();
+		return value;
+
+	}
+
+}
