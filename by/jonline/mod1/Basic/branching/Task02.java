c46afa5239312f6224073e4894f1e7576c233396
AxMakei <97761185+Axm21@users.noreply.github.com>
2022-03-21 02:50:49 +0300

    Add files via upload

---
+++ by/jonline/mod1/Basic/branching/Task02.java
@@ -0,0 +1,39 @@
+package by.jonline.mod1.Basic.branching;
+
+public class Task02 {
+
+	public static void main(String[] args) {
+		// Найти max{min(a, b), min(c, d)}.
+		
+		int a = -5;
+		int b = 5;
+		int c = -9;
+		int d = 9;
+		
+		int min1; // из a,b
+		int min2; // из c,d
+		int max;
+
+
+		if (a >= b)
+			min1 = b;
+		else
+			min1 = a;
+
+		if (c >= d)
+			min2 = d;
+		else
+			min2 = c;
+
+		if (min1 >= min2)
+			max = min1;
+		else
+			max = min2;
+
+		System.out.println("min(a,b) = " + min1);
+		System.out.println("min(c,d) = " + min2);
+		System.out.println("max = " + max);
+
+	}
+
+}
---
+++ by/jonline/mod1/Basic/linear/Task01.java
@@ -0,0 +1,18 @@
+package by.jonline.mod1.Basic.linear;
+
+public class Task01 {
+
+	public static void main(String[] args) {
+		// Найдите значение функции:: z = ( (a � 3 ) * b / 2) + c.
+
+		double a = 15;
+		double b = 5;
+		double c = 3;
+		double z;
+
+		z = ((a - 3) * b / 2 + c);
+
+		System.out.print("z = " + z);
+	}
+
+}
